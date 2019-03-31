93544d234b6d4828b33dde182be0f31ec6bd4263
root <root@localhost.localdomain>
2019-03-31 18:33:48 +0300

    blu

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print("MM")
+#!/usr/bin/env python3.7
+print('MM')
+i=5
+type(i)
+#comment
+'''
+comment
+'''
+dataaa=int(input('please provide data: '))
+print(type(dataaa))
+
+
+if i>6 :
+	print("Good")
+else :
+	print("Bad")
+	
